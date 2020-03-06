5402c8d1c13ccad845e4721d9e9604c5a7aac2a6
Gabriel Siedler <gabrielsiedler@gmail.com>
2020-03-06 14:22:39 -0300

    adding raw to return object

--- mocha/runner.js
+++ mocha/runner.js
@@ -16,7 +16,6 @@ const randomString = (length = 32, chars = '0123456789abcdefghijklmnopqrstuvwxyz
 };
 
 async function runTests(string, options) {
-
   let imports = `const { describe, context, it, specify, before, after, beforeEach, afterEach } = require('mocha'); `;
 
   if (options) {
@@ -26,9 +25,10 @@ async function runTests(string, options) {
         imports = imports + `const { should, expert, assert } = require('chai'); `;
         break;
       case 'assert':
-        imports = imports + `const assert = require('assert'); `; break;
+        imports = imports + `const assert = require('assert'); `;
+        break;
       default:
-        throw `The library '${library}' is not available. Please use 'chai' or 'assert'.`
+        throw `The library '${library}' is not available. Please use 'chai' or 'assert'.`;
         break;
     }
   } else {
@@ -47,26 +47,30 @@ async function runTests(string, options) {
   // If not, create a file from the string
   if (fileExists) {
     // fileName = string;
-    console.err('Please provide a string containing your test code.')
+    console.err('Please provide a string containing your test code.');
   } else {
     fileName = randomString() + '.js';
     if (!directoryExists) fs.mkdirSync(path.join(__dirname, 'temp'));
     fs.writeFileSync(path.join(__dirname, 'temp', fileName), string);
-  };
+  }
 
   test.addFile(path.join(__dirname, 'temp', fileName));
 
   const runner = test.run();
 
   let suiteStart, suiteEnd, runnerStart, runnerEnd;
-  let data = [], summary = {};
+  let data = [],
+    summary = {};
   const set = new Set();
-  let obj = {}, currentSuite = '';
-  let passCount = 0, failCount = 0, nestedLevels = 0, suiteCount = 0, testCount = 0;
-
+  let obj = {},
+    currentSuite = '';
+  let passCount = 0,
+    failCount = 0,
+    nestedLevels = 0,
+    suiteCount = 0,
+    testCount = 0;
 
   return new Promise((resolve, reject) => {
-
     runner.on('suite', (e) => {
       if (!runnerStart) runnerStart = Date.now();
       suiteCount += 1;
@@ -75,13 +79,15 @@ async function runTests(string, options) {
       if (obj.suite) {
         data.push(obj);
         obj = {};
-      };
+      }
 
       const { title, parent } = e;
       if (parent.title !== '') obj.parent_suite = parent.title;
 
       let parentName = parent.title;
-      let nextParent = e.parent, nextTitle, count = 0;
+      let nextParent = e.parent,
+        nextTitle,
+        count = 0;
 
       // Set count based on number of parents
       while (nextTitle !== '') {
@@ -102,7 +108,6 @@ async function runTests(string, options) {
       if (count > nestedLevels) nestedLevels = count;
       if (title) obj.suite = title;
       // if (parent.title) obj.parent = parent.title;
-
     });
 
     runner.on('test end', (e) => {
@@ -116,8 +121,8 @@ async function runTests(string, options) {
           description: title,
           passed: state === 'passed',
         });
-        state === 'passed' ? passCount += 1 : failCount += 1;
-      };
+        state === 'passed' ? (passCount += 1) : (failCount += 1);
+      }
     });
 
     runner.on('suite end', (e) => {
@@ -129,7 +134,7 @@ async function runTests(string, options) {
       if (obj.suite) {
         data.push(obj);
         obj = {};
-      };
+      }
 
       summary.passed = passCount;
       summary.failed = failCount;
@@ -141,11 +146,11 @@ async function runTests(string, options) {
 
       if (!fileExists) {
         fs.unlinkSync(path.join(__dirname, 'temp', fileName), string);
-      };
+      }
 
-      resolve({ summary, data });
+      resolve({ summary, data, raw: obj });
     });
   });
-};
+}
 
 module.exports = runTests;
